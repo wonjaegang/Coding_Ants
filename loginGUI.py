70a4b23f4990fb495fbbab8cd2b4a7e8cdc4048d
Wonjaegang <robinsam@naver.com>
2021-01-25 21:29:52 +0900

    Add login GUI code
    - Cloned from open source. To run this code, you need python3.7 in 32bit, and PyQt5

--- loginGUI.py
+++ loginGUI.py
@@ -1,11 +1,37 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QAxContainer import *
 
 
-# Making PyQt5 instance
-mywindow = QApplication(sys.argv)
-label1 = QLabel("Sentence")
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyStock")
+        self.setGeometry(300, 300, 300, 150)
 
-label1.show()
+        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
 
-mywindow.exec_()
+        btn1 = QPushButton("Login", self)
+        btn1.move(20, 20)
+        btn1.clicked.connect(self.btn1_clicked)
+
+        btn2 = QPushButton("Check state", self)
+        btn2.move(20, 70)
+        btn2.clicked.connect(self.btn2_clicked)
+
+    def btn1_clicked(self):
+        ret = self.kiwoom.dynamicCall("CommConnect()")
+
+    def btn2_clicked(self):
+        if self.kiwoom.dynamicCall("GetConnectState()") == 0:
+            self.statusBar().showMessage("Not connected")
+        else:
+            self.statusBar().showMessage("Connected")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
