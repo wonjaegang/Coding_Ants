4fea7b0015cee7d749ec02b0c336c7c031619e75
Wonjaegang <robinsam@naver.com>
2021-01-27 17:11:43 +0900

    Add modules & main loop

---
+++ PyStockMain.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QAxContainer import *
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.exec_()
