cfdf718c1bd2a3ffc673c5f706b5ddb0ecbe1dc2
Wonjaegang <robinsam@naver.com>
2021-01-30 01:43:14 +0900

    Separate KiwoomAPI class from MainWindow class
    - The MainWindow class had contained too many methods that include the open API operations. This had made the class a lot bigger. So I separated the KiwoomAPI class from the MainWindow class. The tasks related to open API will be processed ONLY inside the KiwoonAPI class.

--- PyStockMain.py
+++ PyStockMain.py
@@ -4,26 +4,71 @@ from PyQt5.QtGui import *
 from PyQt5.QAxContainer import *
 
 
+class KiwoomAPI(QAxWidget):
+    def __init__(self):
+        super().__init__("KHOPENAPI.KHOpenAPICtrl.1")
+        self.searchConditions = {}
+        self.OnReceiveTrCondition.connect(self.receiveSearchResult)
+        self.OnReceiveTrData.connect(self.receiveTrData)
+
+        self.login()
+
+    def login(self):
+        self.dynamicCall("CommConnect()")
+        self.OnEventConnect.connect(self.loginDone)
+
+    def loginDone(self):
+        mainWindow.message.append("Login Succeed")
+        self.saveSearchConditions()
+
+    def saveSearchConditions(self):
+        self.dynamicCall("GetConditionLoad()")
+        self.OnReceiveConditionVer.connect(self.conditionSaved)
+
+    def conditionSaved(self, saved, _):
+        if saved:
+            mainWindow.message.append("Search conditions have been saved.")
+            string = self.dynamicCall("GetConditionNameList()")
+            split_once = string.split(';')
+            split_once.pop()
+            for nameIndex in split_once:
+                split_twice = nameIndex.split('^')
+                self.searchConditions[split_twice[0]] = split_twice[1]
+        else:
+            mainWindow.message.append("Error : Failed to load search conditions")
+
+    def soaredWeakSelling(self):
+        self.sendCondition("000", "0000")
+        pass
+
+    def sendCondition(self, index, screenNo):
+        self.dynamicCall("SendCondition(QString, QString, int, int)",
+                                   screenNo, self.searchConditions[index], index, 0)
+
+    def receiveSearchResult(self, screenNo, codeList, conditionName, index_int, Next_int):
+        if screenNo == "0000":
+            print(codeList)
+
+    def receiveTrData(self, screenNo, requestName, TrCode, recordName, PreNext, _0, _1, _2, _3):
+        pass
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
-        self.searchConditions = {}
+        self.kiwoomAPI = KiwoomAPI()
 
-        # GUI
+        # GUI Definitions
         self.message = QTextEdit(self)
         self.accountInfo = QTextEdit(self)
         self.button1 = QPushButton("1: Loss Cut Scalping", self)
-        self.button1.clicked.connect(self.lossCutScalping)
+        self.button1.clicked.connect(self.run_lossCutScalping)
         self.button1_Stop = QPushButton("Stop Running", self)
-        self.button1_Stop.clicked.connect(self.lossCutScalping_Stop)
+        self.button1_Stop.clicked.connect(self.stop_lossCutScalping)
         self.button2 = QPushButton("2: Soared&WeakSelling", self)
-        self.button2.clicked.connect(self.soared_weakSelling)
+        self.button2.clicked.connect(self.run_soaredWeakSelling)
 
         self.setGUI()
-        self.openLoginWindow()
-        self.kiwoom.OnReceiveTrData.connect(self.receiveTrData)
-        self.kiwoom.OnReceiveTrCondition.connect(self.receiveSearchResult)
 
     def setGUI(self):
         self.setWindowTitle("Coding Ants - Auto stock investing program")
@@ -41,50 +86,18 @@ class MainWindow(QMainWindow):
         self.button1_Stop.setEnabled(False)
         self.button2.setGeometry(350, 100, 160, 30)
 
-    def openLoginWindow(self):
-        self.kiwoom.dynamicCall("CommConnect()")
-        self.kiwoom.OnEventConnect.connect(self.loginDone)
-
-    def loginDone(self):
-        self.message.append("Login Succeed")
-        self.saveSearchConditions()
-
-    def saveSearchConditions(self):
-        self.kiwoom.dynamicCall("GetConditionLoad()")
-        self.kiwoom.OnReceiveConditionVer.connect(self.conditionSaved)
-
-    def conditionSaved(self, saved, _):
-        if saved:
-            self.message.append("Search conditions have been saved.")
-            string = self.kiwoom.dynamicCall("GetConditionNameList()")
-            split_once = string.split(';')
-            split_once.pop()
-            for nameIndex in split_once:
-                split_twice = nameIndex.split('^')
-                self.searchConditions[split_twice[0]] = split_twice[1]
-
-    def lossCutScalping(self):
+    def run_lossCutScalping(self):
         self.message.append("Algorithm Started : Loss Cut Scalping ")
         btn_enable_switch(self.button1_Stop, self.button1)
         pass
 
-    def lossCutScalping_Stop(self):
+    def stop_lossCutScalping(self):
         self.message.append("Algorithm Stopped : Loss Cut Scalping ")
         btn_enable_switch(self.button1, self.button1_Stop)
 
-    def soared_weakSelling(self):
+    def run_soaredWeakSelling(self):
         self.button2.setEnabled(False)
-        self.sendCondition("000", "0000")
-
-    def sendCondition(self, index, screenNo):
-        self.kiwoom.dynamicCall("SendCondition(QString, QString, int, int)",
-                                screenNo, self.searchConditions[index], index, 0)
-
-    def receiveSearchResult(self, screenNo, codeList, conditionName, index_int, Next_int):
-        self.accountInfo.append(codeList)
-
-    def receiveTrData(self, screenNo, requestName, TrCode, recordName, PreNext, _0, _1, _2, _3):
-        pass
+        self.kiwoomAPI.soaredWeakSelling()
 
 
 def btn_enable_switch(btn1, btn2):
